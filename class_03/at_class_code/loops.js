4ed81f373a1bebd34b710f1a65dd26e07e80e5cd
Tetiana Butenko <tetianabutenko@Tetianas-MacBook-Pro.local>
2022-02-10 11:38:15 +1100

    class 3 activities

---
+++ class_03/at_class_code/loops.js
@@ -0,0 +1,41 @@
+// While loops
+// 1. starting point
+// 2. ending point/condition
+// 3. increment/step
+
+
+// count from 10 to 1
+
+let startingPoint = 10;
+
+// while (startingPoint > 0) {
+//     // some code
+//     console.log(startingPoint);
+//     startingPoint = startingPoint - 1;
+// }
+
+// console.clear();
+
+
+// startingPoint = startingPoint - 1;
+// startingPoint -= 1
+// --startingPoint
+// strartingPoint--
+
+// print out every second number from 10 to 1
+
+// while (startingPoint > 0) {
+//     // some code
+//     console.log(startingPoint);
+//     startingPoint -= 2;
+// }
+
+
+// For loop
+
+// count from 10 to 1
+
+for (let count = 10; count > 0; count -= 2) {
+    console.log(count);
+}
+
