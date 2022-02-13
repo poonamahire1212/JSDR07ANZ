bb3e5cc9f7be319696635869535e62a67fa39730
Tetiana Butenko <tetianabutenko@Tetianas-MacBook-Pro.local>
2022-02-13 17:38:01 +1100

    at class 4 code

---
+++ class_04/at_class_code/functions.js
@@ -0,0 +1,21 @@
+
+function sayHello(name, age, country){
+    console.log(`Hello, ${name}! You are ${age} years old and from ${country}`);
+}
+
+// sayHello();
+// sayHello("Dane", 20, "Japan");
+// sayHello(90, "Anna");
+// sayHello("Tanya", 89, "Austria", 80, null, []);
+
+
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+const one = 1;
+const three = 3;
+
+const sum = add(one, three);
+
+console.log(sum);
